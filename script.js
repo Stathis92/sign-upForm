b2a4d64bb979d9e8966360db9b75797642ee01b8
Stathis <133227039+Stathis92@users.noreply.github.com>
2023-10-04 15:56:29 +0300

    Add regex to checkName and checkEmail for validation

--- script.js
+++ script.js
@@ -25,32 +25,52 @@ function checkInputs() {
 }
 
 function checkName(first, last) {
-    //TODO- CHECK NAMES IF THEY HAVE SPECIAL CHARACTERS AND NUMBERS
-  console.log(first, last); //TEMP
+  //Validate first and last Name
+  //regex found in https://www.geeksforgeeks.org/javascript-program-to-check-if-a-string-contains-only-alphabetic-characters/
+  let regex = /^[a-zA-Z]+$/;
+  if(first.match(regex)){
+    console.log("Valid");
+  }
+  else{
+    console.log("Invalid");
+  }
+  if(last.match(regex)){
+    console.log("Valid");
+  }
+  else{
+    console.log("Invalid");
+  }
 }
 
 function checkEmail(mail) {
-    //TODO- CHECK MAIL FORMAT. MAKE SURE IT HAS @ and .
-  console.log(mail); //TEMP
+  //Validate email
+  //regex found in https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
+  let validRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+  
+  if(mail.match(validRegex)){
+    console.log("Valid");
+  }
+  else{
+    console.log("Invalid");
+  }
 }
 
 function checkPhone(phone) {
-    //TODO- idk maybe at least 9 characters???
+  //TODO- idk maybe at least 9 characters???
   console.log(phone); //TEMP
 }
 
 function checkPswd(first) {
-    //TODO- CHECK FOR AT LEAST 6 CHARACTERS, MAYBE ADD MANDATORY NUMBER+SPECIAL CHARACTER
+  //TODO- CHECK FOR AT LEAST 6 CHARACTERS, MAYBE ADD MANDATORY NUMBER+SPECIAL CHARACTER
   console.log(first); //TEMP
   return 1;
 }
 
 function checkMatchingPswd(first, last) {
-    if (first == last)
-        return true;
-    return false;
+  if (first == last) return true;
+  return false;
 }
 
 function submitInfo() {
-    //SEND DATA TOS SERVER
+  //SEND DATA TOS SERVER
 }
